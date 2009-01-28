a367f9f3fb3f3f08541dbb4c578e89400db2a67b
Alan Falloon <alan.falloon@gmail.com>
2009-01-28 11:10:52 -0500

    * test_List.ml: Cleaned up some names and added explanitory comments.

--- test_List.ml
+++ test_List.ml
@@ -3,14 +3,15 @@ open QuickCheck
 let prop_revrev : 'a list -> bool =
   fun xs -> List.rev (List.rev xs) = xs
 
-module AL =
-  Arbitrary_list(Arbitrary_int)
-module SL =
-  PShow_list(PShow_int)
+(* for generating random int lists *)
+module AL = Arbitrary_list(Arbitrary_int) ;;
+(* for printing out int lists *)
+module SL = PShow_list(PShow_int) ;;
+(* for being able to test (int list -> bool) *)
 module Testable_list_to_bool =
   Testable_fun
     (AL)
     (SL)
-    (Testable_bool)
+    (Testable_bool) ;;
 module C = Check(Testable_list_to_bool)
 let () = C.quickCheck prop_revrev
